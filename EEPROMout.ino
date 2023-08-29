e8068762f77e984996b533fd858e32f5a3c73f6e
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2023-08-29 11:28:48 -0400

    Update EEPROMout.ino

--- EEPROMout.ino
+++ EEPROMout.ino
@@ -2,7 +2,6 @@
 
 void setup() {
   Serial.begin(9600);	/* Define baud rate for serial communication */
-  // put your setup code here, to run once:
   Serial.println("--- BEGIN DATA DISPLAY ---");
   for (int i = 0; i < EEPROM.length(); i++)
     Serial.println(EEPROM.read(i));
