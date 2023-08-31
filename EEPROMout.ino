9ae0b0d27729513ceb2e173f6197c84af4573600
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2023-08-31 15:42:44 -0400

    Added better formatting and a bug fix to EEPROMout.ino

--- EEPROMout.ino
+++ EEPROMout.ino
@@ -1,12 +1,18 @@
 #include <EEPROM.h>
 
 void setup() {
-  Serial.begin(9600);	/* Define baud rate for serial communication */
+  unsigned int i;
+  Serial.begin(9600); /* Define baud rate for serial communication */
+  
   Serial.println("--- BEGIN DATA DISPLAY ---");
-  for (int i = 0; i < EEPROM.length(); i++)
+  for (int i = 0; i < EEPROM.length(); i++) {
+    Serial.print(i);
+    Serial.print(", ");
     Serial.println(EEPROM.read(i));
+  }
   Serial.println("--- END DATA DISPLAY ---");
 }
 
 void loop() {
+  // Nothing to do in the loop for this example
 }
